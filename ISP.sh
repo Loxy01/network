a7345074c132876fc770737b8d2d494fa84d4a3e
Andrey <99467714+Loxy01@users.noreply.github.com>
2024-10-22 09:39:20 +0300

    Update ISP.sh

--- ISP.sh
+++ ISP.sh
@@ -4,8 +4,8 @@ apt-get install tzdata -y
 timedatectl set-timezone Europe/Moscow
 apt-get install iptables -y
 mkdir /etc/net/ifaces/enp6s{19,20}
-cp enp6s18/options enp6s19/
-cp enp6s18/options enp6s20/
+cp /etc/net/ifaces/enp6s18/options enp6s19/
+cp /etc/net/ifaces/enp6s18/options enp6s20/
 echo -e "BOOTPROTO=static\nTYPE=eth" /etc/net/ifaces/enp6s19/options
 echo -e "BOOTPROTO=static\nTYPE=eth" /etc/net/ifaces/enp6s20/options
 touch /etc/net/ifaces/enp6s19/ipv4address
@@ -13,8 +13,6 @@ touch /etc/net/ifaces/enp6s20/ipv4address
 echo net.ipv4.ip_forward = 1 >> /etc/sysctl.conf
 cp /etc/net/sysctl.conf /etc/net/sysctl.conf.bak
 sed -i 's/net.ipv4.ip_forward = 0/net.ipv4.ip_forward = 1/g' /etc/net/sysctl.conf
-cp /etc/ffr/daemons /etc/frr/daemons.bak
-sed -i 's/osfpd=no/osfpd=yes/g' /etc/frr/daemons
 iptables -t nat -A POSTROUTING -o enp6s18 -j MASQUERADE
 iptables-save > /etc/sysconfig/iptables
 hostnamectl set-hostname isp.au-team.irpo
