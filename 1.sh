7a5ac6694be32111b932b82c9f29f7cc0b61dd61
Andrey <99467714+Loxy01@users.noreply.github.com>
2025-06-16 09:40:31 +0300

    Update to network.sh

--- 1.sh
+++ 1.sh
@@ -2,6 +2,6 @@
 
 remote_user="user"
 remote_host="172.16.4.2"
-remote_script="/home/user/SH/HQ-RTR.sh"
+remote_script="/home/user/network/HQ-RTR.sh"
 
 ssh $remote_user@$remote_host "sudo $remote_script"
